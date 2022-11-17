e9b7b94d18a5dded660805edd33158550978a21a
jewcomm <navr.a@yandex.ru>
2022-11-17 22:05:58 +0300

    Try rewrite on SFML matrix

--- main.cpp
+++ main.cpp
@@ -75,12 +75,12 @@ std::vector<float> multVecOnMatrix(std::vector<float>v, std::vector<std::vector<
     v.push_back(1);
     for(int i = 0; i < m.size(); i++){
         float temp = 0;
-        for(int j = 0; j < v.size(); j++){
+        for(int j = 0; j < m.size(); j++){
             temp += m[i][j] * v[j];
         }
         res.push_back(temp);
     }
-    v.pop_back();
+    res.pop_back();
     return res;
 }
 
@@ -98,40 +98,7 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(sizeX, sizeY), "Lab's 1 graphic affine processing");
 
-    /*
-    std::vector<std::vector<float>> aftR1;
-    for(int i = 0; i < points.size(); i++){
-        std::vector<float> res = mult(points[i], rotate);
-        //std::vector<float> res = mult(std::vector<float>({3,2,1}), points);
-        aftR1.push_back(res);
-    }
-
-    std::vector<std::vector<float>> aftR2;
-    for(int i = 0; i < aftR1.size(); i++){
-        std::vector<float> res = mult(aftR1[i], rabs);
-        //std::vector<float> res = mult(std::vector<float>({3,2,1}), points);
-        aftR2.push_back(res);
-    }
-
-    std::vector<std::vector<float>> aftR;
-    for(int i = 0; i < aftR2.size(); i++){
-        std::vector<float> res = mult(aftR2[i], rabsciss);
-        //std::vector<float> res = mult(std::vector<float>({3,2,1}), points);
-        aftR.push_back(res);
-    }
-
-    */
-//    for(int i = 0; i < points.size(); i++){
-//        std::vector<float> t;
-//        float x = aftR[i][0];
-//        float y = aftR[i][1];
-//        float z = aftR[i][2];
-//        float mX = sizeX / 2 + x * dist / (z + dist);
-//        float mY = sizeY / 2 - y * dist / (z + dist);
-//        t.push_back(mX);
-//        t.push_back(mY);
-//        pointInNewCoord.push_back(t);
-//    }
+    window.setFramerateLimit(60);
 
     float t = 0;
 
@@ -144,42 +111,53 @@ int main()
                 window.close();
         }
 
-        std::vector<std::vector<float>> f;
+//        std::vector<std::vector<float>> f;
+//
+//        for(int i = 0; i < points.size(); i++){
+//            f.push_back(multVecOnMatrix(points[i], rotateApp(t)));
+//        }
+//
+//        std::vector<std::vector<float>> k;
+//
+//        for(int i = 0; i < f.size(); i++){
+//            k.push_back(multVecOnMatrix(f[i], rotateAbs(t)));
+//        }
+//
+//        std::vector<std::vector<float>> g;
+//
+//        for(int i = 0; i < k.size(); i++){
+//            g.push_back(multVecOnMatrix(k[i], rotateOrd(t)));
+//        }
+//
+//        std::vector<std::vector<float>> r = convert3Dto2D(g);
+//
+//        t += 0.003;
+//        if(t > 360) t = 0;
+
+        std::vector<std::vector<float>> newBasis;
+
+//        for(int i = 0; i < basis.size(); i++){
+//            newBasis.push_back(multVecOnMatrix(basis[i], rotateAbs(30)));
+//        }
+
+        newBasis = basis;
+
+        std::vector<std::vector<float>> inNewBasis;
 
         for(int i = 0; i < points.size(); i++){
-            f.push_back(multVecOnMatrix(points[i], rotateApp(t)));
+            inNewBasis.push_back(multVecOnMatrix(points[i], newBasis));
         }
 
-        std::vector<std::vector<float>> k;
-
-        for(int i = 0; i < f.size(); i++){
-            k.push_back(multVecOnMatrix(f[i], rotateAbs(t)));
-        }
-
-        std::vector<std::vector<float>> g;
-
-        for(int i = 0; i < k.size(); i++){
-            g.push_back(multVecOnMatrix(k[i], rotateOrd(t)));
-        }
-
-        std::vector<std::vector<float>> r = convert3Dto2D(g);
-
-        t += 0.003;
-        if(t > 360) t = 0;
+        inNewBasis = convert3Dto2D(inNewBasis);
 
         window.clear();
-        for (int i = 0; i < r.size() - 1; i++){
+        for (int i = 0; i < inNewBasis.size() - 1; i++){
             sf::Vertex line[] = {
-                    sf::Vertex(sf::Vector2f(r[i][0], r[i][1])),
-                    sf::Vertex(sf::Vector2f(r[i + 1][0], r[i + 1][1]))
+                    sf::Vertex(sf::Vector2f(inNewBasis[i][0], inNewBasis[i][1])),
+                    sf::Vertex(sf::Vector2f(inNewBasis[i + 1][0], inNewBasis[i + 1][1]))
             };
             window.draw(line, 2, sf::Lines);
         }
-//        sf::Vertex line[] = {
-//                sf::Vertex(sf::Vector2f(basisX + points[points.size() - 1][0], basisY + points[points.size() - 1][1])),
-//                sf::Vertex(sf::Vector2f(basisX + points[0][0], basisY + points[0][1]))
-//        };
-//        window.draw(line, 2, sf::Lines);
         window.display();
     }
     return 0;
