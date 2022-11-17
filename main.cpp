e68aab5bb93a70fe4be53d143fdff71aab54a667
jewcomm <navr.a@yandex.ru>
2022-11-17 21:36:45 +0300

    from coord to basis

--- main.cpp
+++ main.cpp
@@ -2,96 +2,103 @@
 #include <vector>
 #include <cmath>
 
-//std::vector<std::vector<float>> points = {
-//                                        {150, 150, 0},
-//                                        {250, 250, 0},
-//                                        {250, 150, 0},
-//                                        {250, 250, 100},
-//                                        {150, 150, 100},
-//                                        {150, 250, 100}
-//};
-
 std::vector<std::vector<float>> points = {
-        {100, 0, 0},
-        {100, 100, 0},
-        {0, 100, 0},
-        {0, 0, 0},
-        {100, 0, 0},
-        {0, 0, 0},
+        //X     Y       Z
+        {100,   0,      0},
+        {100,   100,    0},
+        {0,     100,    0},
+        {0,     0,      0},
+        {100,   0,      0},
+        {0,     0,      0},
 
-        {0, 0, 100},
-        {0, 100, 100},
-        {100, 100, 100},
-        {100, 0, 100},
+        {0,     0,      100},
+        {0,     100,    100},
+        {100,   100,    100},
+        {100,   0,      100},
+        {0,     0,      100},
+        {100,   0,      100},
 
-        {100, 0, 0},
-        {100, 100, 0},
-        {100, 100, 100},
-        {0, 100, 100},
-        {0, 100, 0},
-        {0, 0, 0},
-        {0, 0, 100}
+        {100,   0,      0},
+        {100,   100,    0},
+        {100,   100,    100},
+        {0,     100,    100},
+        {0,     100,    0},
+        {0,     0,      0},
+        {0,     0,      100}
 };
 
-//std::vector<std::vector<float>> points = {
-//        {1, 0, 0, 0},
-//        {0, 1, 0, 0},
-//        {0, 0, 1, 0},
-//        {0, 0, 0, 1}
-//};
-
-std::vector<std::vector<float>> pointInNewCoord;
-
-float basisX = 0;
-float basisY = 0;
+std::vector<std::vector<float>> basis = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 1}
+};
 
 float sizeX = 1000;
 float sizeY = 1000;
 
 float dist = 1000;
 
-float grad = 30;
-float gradSin = sinf(grad * M_PI / 180);
-float gradCos = cosf(grad * M_PI / 180);
+std::vector<std::vector<float>> rotateAbs(float angle){
+    float sinFI = sinf(angle * M_PI / 180);
+    float cosFI = cosf(angle * M_PI / 180);
+    return {{1, 0, 0, 1},
+            {0, cosFI, sinFI, 0},
+            {0, 0-sinFI, cosFI, 0},
+            {0, 0, 0, 1}};
+}
 
-std::vector<std::vector<float>> rotate = {
-        {gradCos, 0, 0-gradSin, 0},
-        {0, 1, 0, 0},
-        {gradSin, 0, gradCos, 0},
-        {0, 0, 0, 1}
-};
+std::vector<std::vector<float>> rotateOrd(float angle){
+    float sinFI = sinf(angle * M_PI / 180);
+    float cosFI = cosf(angle * M_PI / 180);
+    return {
+            {cosFI, sinFI, 0, 0},
+            {0-sinFI, cosFI, 0, 0},
+            {0, 0, 1, 0},
+            {0, 0, 0, 1}
+    };
+}
 
-std::vector<std::vector<float>> rabs = {
-        {gradCos, gradSin, 0, 0},
-        {0-gradSin, gradCos, 0, 0},
-        {0, 0, 1, 0},
-        {0, 0, 0, 1}
-};
+std::vector<std::vector<float>> rotateApp(float angle){
+    float sinFI = sinf(angle * M_PI / 180);
+    float cosFI = cosf(angle * M_PI / 180);
+    return {
+            {cosFI, 0, 0-sinFI, 0},
+            {0, 1, 0, 0},
+            {sinFI, 0, cosFI, 0},
+            {0, 0, 0, 1}
+    };
+}
 
-std::vector<std::vector<float>> rabsciss = {
-        {1, 0, 0, 1},
-        {0, gradCos, gradSin, 0},
-        {0, 0-gradSin, gradCos, 0},
-        {0, 0, 0, 1}
-};
 
-std::vector<float> mult(std::vector<float>v, std::vector<std::vector<float>> m){
+std::vector<float> multVecOnMatrix(std::vector<float>v, std::vector<std::vector<float>> m){
     std::vector<float> res;
     v.push_back(1);
-    for(int i = 0; i < v.size(); i++){
+    for(int i = 0; i < m.size(); i++){
         float temp = 0;
         for(int j = 0; j < v.size(); j++){
             temp += m[i][j] * v[j];
         }
         res.push_back(temp);
     }
+    v.pop_back();
     return res;
+}
+
+std::vector<std::vector<float>> convert3Dto2D(std::vector<std::vector<float>> input){
+    std::vector<std::vector<float>> result;
+    for(int i = 0; i < input.size(); i++){
+        float mX = sizeX / 2 + input[i][0] * dist / (input[i][3] + dist);
+        float mY = sizeY / 2 + input[i][1] * dist / (input[i][3] + dist);
+        result.push_back(std::vector<float>({mX, mY}));
+    }
+    return result;
 }
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(sizeX, sizeY), "Lab's 1 graphic affine processing");
 
+    /*
     std::vector<std::vector<float>> aftR1;
     for(int i = 0; i < points.size(); i++){
         std::vector<float> res = mult(points[i], rotate);
@@ -101,7 +108,6 @@ int main()
 
     std::vector<std::vector<float>> aftR2;
     for(int i = 0; i < aftR1.size(); i++){
-        aftR1[i].pop_back();
         std::vector<float> res = mult(aftR1[i], rabs);
         //std::vector<float> res = mult(std::vector<float>({3,2,1}), points);
         aftR2.push_back(res);
@@ -109,23 +115,25 @@ int main()
 
     std::vector<std::vector<float>> aftR;
     for(int i = 0; i < aftR2.size(); i++){
-        aftR2[i].pop_back();
-        std::vector<float> res = mult(aftR2[i], rabs);
+        std::vector<float> res = mult(aftR2[i], rabsciss);
         //std::vector<float> res = mult(std::vector<float>({3,2,1}), points);
         aftR.push_back(res);
     }
 
-    for(int i = 0; i < points.size(); i++){
-        std::vector<float> t;
-        float x = aftR[i][0];
-        float y = aftR[i][1];
-        float z = aftR[i][2];
-        float mX = sizeX / 4 + x * dist / (z + dist);
-        float mY = sizeY / 2 - y * dist / (z + dist);
-        t.push_back(mX);
-        t.push_back(mY);
-        pointInNewCoord.push_back(t);
-    }
+    */
+//    for(int i = 0; i < points.size(); i++){
+//        std::vector<float> t;
+//        float x = aftR[i][0];
+//        float y = aftR[i][1];
+//        float z = aftR[i][2];
+//        float mX = sizeX / 2 + x * dist / (z + dist);
+//        float mY = sizeY / 2 - y * dist / (z + dist);
+//        t.push_back(mX);
+//        t.push_back(mY);
+//        pointInNewCoord.push_back(t);
+//    }
+
+    float t = 0;
 
     while (window.isOpen())
     {
@@ -136,11 +144,34 @@ int main()
                 window.close();
         }
 
+        std::vector<std::vector<float>> f;
+
+        for(int i = 0; i < points.size(); i++){
+            f.push_back(multVecOnMatrix(points[i], rotateApp(t)));
+        }
+
+        std::vector<std::vector<float>> k;
+
+        for(int i = 0; i < f.size(); i++){
+            k.push_back(multVecOnMatrix(f[i], rotateAbs(t)));
+        }
+
+        std::vector<std::vector<float>> g;
+
+        for(int i = 0; i < k.size(); i++){
+            g.push_back(multVecOnMatrix(k[i], rotateOrd(t)));
+        }
+
+        std::vector<std::vector<float>> r = convert3Dto2D(g);
+
+        t += 0.003;
+        if(t > 360) t = 0;
+
         window.clear();
-        for (int i = 0; i < pointInNewCoord.size() - 1; i++){
+        for (int i = 0; i < r.size() - 1; i++){
             sf::Vertex line[] = {
-                    sf::Vertex(sf::Vector2f(basisX + pointInNewCoord[i][0], basisY + pointInNewCoord[i][1])),
-                    sf::Vertex(sf::Vector2f(basisX + pointInNewCoord[i + 1][0], basisY + pointInNewCoord[i + 1][1]))
+                    sf::Vertex(sf::Vector2f(r[i][0], r[i][1])),
+                    sf::Vertex(sf::Vector2f(r[i + 1][0], r[i + 1][1]))
             };
             window.draw(line, 2, sf::Lines);
         }
