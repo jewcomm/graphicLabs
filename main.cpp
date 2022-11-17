ed0549603f59aba9688e36b44c542afb814b5431
jewcomm <navr.a@yandex.ru>
2022-11-17 23:42:48 +0300

    Calculate inverse matrix with Transform

--- main.cpp
+++ main.cpp
@@ -84,6 +84,20 @@ std::vector<float> multVecOnMatrix(std::vector<float>v, std::vector<std::vector<
     return res;
 }
 
+std::vector<float> multMatrixOnVec(std::vector<std::vector<float>> m, std::vector<float>v){
+    std::vector<float> res;
+    v.push_back(1);
+    for(int i = 0; i < m.size(); i++){
+        float temp = 0;
+        for(int j = 0; j < m.size(); j++){
+            temp += m[j][i] * v[j];
+        }
+        res.push_back(temp);
+    }
+    res.pop_back();
+    return res;
+}
+
 std::vector<std::vector<float>> convert3Dto2D(std::vector<std::vector<float>> input){
     std::vector<std::vector<float>> result;
     for(int i = 0; i < input.size(); i++){
@@ -92,6 +106,29 @@ std::vector<std::vector<float>> convert3Dto2D(std::vector<std::vector<float>> in
         result.push_back(std::vector<float>({mX, mY}));
     }
     return result;
+}
+
+std::vector<std::vector<float>> inverseMatrix(std::vector<std::vector<float>> matrix){
+    sf::Transform source(matrix[0][0], matrix[0][1], matrix[0][2],
+                         matrix[1][0], matrix[1][1], matrix[1][2],
+                         matrix[2][0], matrix[2][1], matrix[2][2]);
+
+    sf::Transform inverse = source.getInverse();
+    float *r = (float*)inverse.getMatrix();
+    std::vector<float> revers(r, r+16);
+
+    std::vector<std::vector<float>> ret;
+
+    for(int i = 0; i < 4; i++){
+        if(i == 2) continue;
+        std::vector<float> temp;
+        for(int j = 0; j < 4; j++){
+            if(j != 2) temp.push_back(revers[i * 4 + j]);
+        }
+        ret.push_back(temp);
+    }
+
+    return ret;
 }
 
 int main()
@@ -111,44 +148,23 @@ int main()
                 window.close();
         }
 
-//        std::vector<std::vector<float>> f;
-//
-//        for(int i = 0; i < points.size(); i++){
-//            f.push_back(multVecOnMatrix(points[i], rotateApp(t)));
-//        }
-//
-//        std::vector<std::vector<float>> k;
-//
-//        for(int i = 0; i < f.size(); i++){
-//            k.push_back(multVecOnMatrix(f[i], rotateAbs(t)));
-//        }
-//
-//        std::vector<std::vector<float>> g;
-//
-//        for(int i = 0; i < k.size(); i++){
-//            g.push_back(multVecOnMatrix(k[i], rotateOrd(t)));
-//        }
-//
-//        std::vector<std::vector<float>> r = convert3Dto2D(g);
-//
-//        t += 0.003;
-//        if(t > 360) t = 0;
-
         std::vector<std::vector<float>> newBasis;
 
-//        for(int i = 0; i < basis.size(); i++){
-//            newBasis.push_back(multVecOnMatrix(basis[i], rotateAbs(30)));
-//        }
+        for(int i = 0; i < basis.size(); i++){
+            newBasis.push_back(multMatrixOnVec(rotateAbs(t), basis[i]));
+        }
 
-        newBasis = basis;
+        std::vector<std::vector<float>> inverseNewBasis = inverseMatrix(newBasis);
 
         std::vector<std::vector<float>> inNewBasis;
 
         for(int i = 0; i < points.size(); i++){
-            inNewBasis.push_back(multVecOnMatrix(points[i], newBasis));
+            inNewBasis.push_back(multMatrixOnVec(inverseNewBasis, points[i]));
         }
 
         inNewBasis = convert3Dto2D(inNewBasis);
+
+        t += 0.03;
 
         window.clear();
         for (int i = 0; i < inNewBasis.size() - 1; i++){
